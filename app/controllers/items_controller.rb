1673ea74843123de4813af25d5c9f89e7ce4a372
ten-ryu <amagasa_ryuto@icloud.com>
2020-09-07 18:05:23 +0900

    javascriptの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new, :create]
 
   def index
   end
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :details, :category_id, :condition_id, :delivery_fee_id, :ship_area_id, :ship_days_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item,).permit(:name, :details, :category_id, :condition_id, :delivery_fee_id, :ship_area_id, :ship_days_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
  
-  validates :name, :details, :category, :condition, :delivery_fee, :ship_area, :ship_days, :price, presence: true
-  
+  validates :image, :name, :details, :category, :condition, :delivery_fee, :ship_area, :ship_days, :price, presence: true
   validates :category_id, :condition_id, :delivery_fee_id, :ship_area_id, :ship_days_id, numericality: { other_than: 1 }
+  validates :price, format:{with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."},inclusion:{in: 300..9999999,message: "is out of setting range"}
 end
