070d381fad311362d53e5a394e45f80e6b989ccd
ten-ryu <amagasa_ryuto@icloud.com>
2020-09-11 12:36:42 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   def show
     @item = Item.find(params[:id])
   end
