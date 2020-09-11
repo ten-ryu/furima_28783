265626f56e4d1dc2e5f9de45d5cdaf490ffa65ec
ten-ryu <amagasa_ryuto@icloud.com>
2020-09-11 14:39:28 +0900

    商品削除機能(修正②)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,8 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+     redirect_to root_path
+    else
+      render :show
+    end
   end
 
   def show
