7fc2b9f8e87a8c665729b805abd52e860061647f
ten-ryu <amagasa_ryuto@icloud.com>
2020-09-17 11:49:37 +0900

    商品購入機能(バリデーション)修正③

--- app/forms/address_management.rb
+++ app/forms/address_management.rb
@@ -6,7 +6,7 @@ class AddressManagement
   validates :token,:postcode, :prefecture_id, :city, :address, :phone_number,  presence: true
   validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :phone_number, format: {with: /\A\d{11}\z/}
+  validates :phone_number, format: {with: /\A\d{11}\z/},length: { maximum: 11 }
   
   def save
     management = Management.create(user_id: user_id, item_id: item_id)
